c62371fff362776d779638933cd04e0a234bdd18
DanNgo4 <ducdanh2004@gmail.com>
2023-05-16 17:47:16 +1000

    .

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+	$host   = "feenix-mariadb.swin.edu.au";
+	$user   = "s"; // your user name
+	$pwd    = ""; // your password (date of birth ddmmyy unless changed)
+	$sql_db = ""; // your database
+    $conn   = @mysqli_connect ($host,
+                               $user,
+                               $pwd,
+                               $sql_db
+    );
+?>
