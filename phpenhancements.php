5146054885b81594023a771c4fef8810b9fd2b39
DanNgo4 <ducdanh2004@gmail.com>
2023-05-28 14:32:21 +1000

    Create phpenhancements.php

---
+++ phpenhancements.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="utf-8" />
+    <meta name = "description" content = "SOE Manager Login" />
+    <meta name = "keywords"    content = "PHP, MySQL, SOE, Manager" />        
+    <meta name = "author"      content = "Dan Ngo" />
+   <title>Manager Login</title>
+   </head>
+<body>
+<?php
+    include 'Header.inc'
+?>
+    <form >
+    </form>
+<?php
+    include 'Footer.inc'
+?>
+</body>    
+</html>
