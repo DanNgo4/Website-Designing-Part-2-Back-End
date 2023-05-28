9c553ac107f42597c4189bc82e04771375da907a
DanNgo4 <ducdanh2004@gmail.com>
2023-05-28 13:48:07 +1000

    a

---
+++ References.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="styles/style.css">
+
+<head>
+  <meta charset="UTF-8">
+  <title>processEOI</title>
+</head>
+
+<body>
+<?php 
+	include 'Header.inc'; 
+?>
+    <p>This line is intentionally left here</p>
+
+
+<?php 
+	include 'Footer.inc'; 
+?>
+  
+</body>
+</html>
