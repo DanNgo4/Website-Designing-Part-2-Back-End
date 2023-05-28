86cbd595f61ac49f27f2ab86ca332946fcdb7aa0
DanNgo4 <ducdanh2004@gmail.com>
2023-05-28 16:41:52 +1000

    a

--- manage.php
+++ manage.php
@@ -10,52 +10,7 @@
 </head>
 <body>
 <?php
-session_start();
     include ("Header.inc");
-    // Database connection
-    require("settings.php");
-    //Checks if connection is successful
-    if (!$conn) {
-        //displays an error message 
-        echo "<p>Database connection failure</p>";
-    } 
-
-    // Manager login validation
-    if ((isset($_POST["name"])) and (isset($POST["pwd"]))) {
-        $name = sanitizeInput($_POST["name"]);
-        $pwd  = sanitizeInput($_POST["pwd"]);
-
-        if (empty($name)) {
-            header("Location: phpenhancements.php?error=Name is required");
-            exit();
-        }
-        elseif (empty($pwd)) {
-            header("Location: phpenhancements.php?error=Password is required");
-            exit();
-        }
-        else {
-            $sql = "SELECT * FROM manager WHERE name = '$name' AND password = '$pwd'";
-            $res = @mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($res) === 1) {
-                $row = mysqli_fetch_assoc($res);
-                if ($row["name"] === $name and $row["password"] === $pwd) {
-                    $SESSION["name"] = $row["name"];
-                    $SESSION["name"] = $row["name"];
-                    header("Location: manage.php");
-                    exit();
-                }
-                else {
-                    header("Location: phpenhancements.php?error=Incorrect name or password");
-                    exit();
-                }
-            }
-        }
-    }
-    else {
-        header("Location: phpenhancements.php?error");
-        exit();
-    }
 ?>
         <h1>EOI Management</h1>
 
