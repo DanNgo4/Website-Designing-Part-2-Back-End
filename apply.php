829a7d1077a57adfa94830ea742f19402589bdce
Mandeep07Kaur <mandeep53kaurkahlon@gmail.com>
2023-05-28 14:38:18 +1000

    changes made to EOI and Process OI

--- apply.php
+++ apply.php
@@ -25,31 +25,29 @@
         <fieldset>
             <!--Job reference with a text area max inputs 5 alphanumeric characters-->
             <p><label for="JRN">Job Reference Number</label>
-                <input type="text" name="Job Reference Number" id="JRN" maxlength="5" pattern="^[a-zA-Z0-9]{1,5}" required="required"></p>
+                <input type="text" name="Job_Reference" id="Job_Reference" maxlength="5" pattern="^[a-zA-Z0-9]{1,5}" required="required"></p>
         </fieldset>
 	<fieldset>
         <!--legend{font-family: serif ;color:black; font-size: 1.5em;}
 -->
         <legend class="heading-apply">Personal details</legend>
 	<p><label for="Firstname"> First Name</label>
-        <input type="text" name="Name[]" id="Firstname" Maxlength="20" pattern="[a-zA-Z]{1,20}" required="required">
+        <input type="text" name="First_Name" id="First_Name" Maxlength="20" pattern="[a-zA-Z]{1,20}" required="required">
 	</p>
     <!--ensure proper gap between labels-->
-	<p><label for="Middlename">Middle Name*</label>
-        <input type="text" name="Name[]" id="Middlename" pattern="[a-zA-Z]{1,20}">
-    </p>
+	
     <p><label for="Lastname">Last Name</label>
-        <input type="text" name="Name[]" id="Lastname" maxlength="20" pattern="[A-Za-z]{1,20}" required="required">
+        <input type="text" name="Last_Name" id="Last_Name" maxlength="20" pattern="[A-Za-z]{1,20}" required="required">
         </p>
         <p><label for="dob">DOB</label>
-            <input type="date" name="DOB" id="dob" >
+            <input type="date" name="dob" id="dob" >
             <p><label for="street">Steet Address</label>
-            <input type="text" name="address[]" id="street" maxlength="40" pattern="^[a-zA-Z0-9]{1,40}+$" required="required">
+            <input type="text" name="Street_Address" id="Street_Address" maxlength="40" pattern="^[a-zA-Z0-9]{1,40}+$" required="required">
             <label for="suburb">Suburb/Town</label>
-            <input type="text" name="address[]" id="suburb" maxlength="40" pattern="^[a-zA-Z0-9]{1,40}+$" required="required">
+            <input type="text" name="Suburb_Town" id="Suburb_Town" maxlength="40" pattern="^[a-zA-Z0-9]{1,40}+$" required="required">
             <!--States in drop down-->
             <label for="State">State</label>
-            <select name="address[]" id="State">
+            <select name="State" id="State">
                 <option selected="selected">Please Select</option>
                 <option value="VIC">VIC</option>
                 <option value="NSW">NSW</option>
@@ -61,7 +59,7 @@
                 <option value="ACT">ACT</option>
             </select>
             <label for="post">Post Code</label>
-            <input type="text" name="address[]" id="post" maxlength="4" pattern="\d{4}" required="required">
+            <input type="text" name="Postcode" id="Postcode" maxlength="4" pattern="\d{4}" required="required">
             </p></fieldset>
             <fieldset>
                 <!--Gender with radio inputs-->
@@ -81,40 +79,15 @@
             <fieldset>
                 <legend class="heading-apply">Contact Details</legend>
                 <p><label for="Phone">Phone Number</label>
-                <input type="text" name="contact[]" id="Phone" pattern="[\d\s]{8,12}" required="required">
-                <label for="Tele">Alternative Phone number</label>
-                <input type="text" name="contact[]" id="Tele" pattern="[\d\s]{8,12}" required="required">
+                <input type="text" name="Phone_Number" id="Phone_Number" pattern="[\d\s]{8,12}" required="required">
                 <label for="email">E-mail Address</label>
-                <input type="email" name="contact[]" id="email" required="required">
+                <input type="email" name="Email_Address" id="Email_Address" required="required">
                 </p>
             </fieldset>
-            <fieldset>
-                <legend class="heading-apply">Educational Background</legend>
-                <p><label for="school">School</label>
-                    <input type="text" name="edu[]" id="school" pattern="[a-zA-Z]{1,40}" required="required"></p>
-                    <label for="uni">College/University*</label>
-                    <p><input type="text" name="edu[]" id="uni" placeholder="OPTIONAL" pattern="[a-zA-Z]{1,40}"></p>
-                    <p><label for="deg">Degree*</label>
-                    <input type="text" name="edu[]" id="deg" placeholder="If Any" pattern="[a-zA-Z]{1,40}"></p>
-                 </fieldset>
+            
                  <fieldset>
                     <legend class="heading-apply">Skills and Experience</legend>
-                    <p>Currently Employed</p>
-                    <p><label for="Yes">Yes</label>
-                     <input type="radio" value="Yes" name="Currently Employed" id="Yes" required="required">
-                     <label for="No">No</label>
-                     <input type="radio" value="No" name="Currently Employed" id="No" required="required"></p>
-                     <p><label for="Experience">Experience</label>
-                        <select name="Experience" id="Experience">
-                        <option selected="selected">Please Select</option>
-                        <option value="less than 1 year">Less Than 1 Year</option>
-                        <option value="1-2">1-2 Years</option>
-                        <option value="2-5">2-5 Years</option>
-                        <option value="5+">5+ Years</option>
-                    </select></p>
-                    <p><label for="Licence">Licence*</label>
-                        <input type="text" name="Licence" id="Licence" placeholder="If Any" pattern="^[a-zA-Z0-9]{1,40}+$"></p>
-                        <p>Skills</p>
+                    <p>Skills</p>
                         <p><label for="OOP">Object Oriented programming</label>
                             <input type="checkbox" value="OOP" name="Skills[]" id="OOP" >
                         <label for="APIs">APIs</label>
@@ -130,18 +103,10 @@
                         </p>
 
                            <p><label for="OTS">Other Skills</label>
-                             <textarea id="OTS" name="Description of Other Skills" rows="4" cols="40" placeholder="Mention your skills here"></textarea></p>
+                             <textarea id="OtherSkills" name="Description of Other Skills" rows="4" cols="40" placeholder="Mention your skills here"></textarea></p>
 
                     </fieldset>
-                    <fieldset>
-                        <legend class="heading-apply">Reference</legend>
-                        <p><label for="Name">Name*</label>
-                            <input type="text" name="Name" id="Name" pattern="^[a-zA-Z]+$"></p>
-                            <p><label for="Number">Contact Number*</label>
-                            <input type="text" name="Number" id="Number" pattern="[\d\s]{8,12}">
-
-
-                             </fieldset>
+                    
             <!-- Jugraj Adding buttons -->
             <button class="btn02"><input class="btnapy" type="submit" value="Apply"></button>
             <button class="btn02"><input class="btnapy" type="reset" value="Reset Form"></button>
--- processEOI.php
+++ processEOI.php
@@ -10,7 +10,7 @@
     <?php
 // Server-side validation and processing
 
-// This function ensures All data should be sanitized to remove leading and trailing spaces, backslashes and HTML control characters. 
+// This function ensures all data should be sanitized to remove leading and trailing spaces, backslashes, and HTML control characters.
 function sanitise_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -18,38 +18,40 @@ function sanitise_input($data) {
   return $data;
 }
 
-
 // Connect to the database
-require_once ("settings.php");
+require_once("settings.php");
 
-    $conn = @mysqli_connect($host,
+$conn = @mysqli_connect(
+    $host,
     $user,
     $pwd,
     $sql_db
 );
- // Check connection,if failed;displays an Error message
- if (!$conn) {
-  echo "<p>Database connection failure</p>";
-  }
+// Check connection; if failed, display an error message
+if (!$conn) {
+    echo "<p>Database connection failure</p>";
+}
 
 
-  // Validate and sanitize form inputs
- 
-    if (isset($_POST["Job_Reference"])) {
-        $Job_Reference = sanitise_input($_POST["Job_Reference"]);
-    } else {
-        header("location: apply.php");
-        exit;
-    }
+// Validate and sanitize form inputs
 
-    if (isset($_POST["First_Name"])) {
-      $First_Name = sanitise_input($_POST["First_Name"]);
-  } else {
-      header("location: apply.php");
-      exit;
-  }
+$errMsg = ""; // Added variable to store error messages
 
-  if (isset($_POST["Last_Name"])) {
+if (isset($_POST["Job_Reference"])) {
+    $Job_Reference = sanitise_input($_POST["Job_Reference"]);
+} else {
+    header("location: apply.php");
+    exit;
+}
+
+if (isset($_POST["First_Name"])) {
+    $First_Name = sanitise_input($_POST["First_Name"]);
+} else {
+    header("location: apply.php");
+    exit;
+}
+
+if (isset($_POST["Last_Name"])) {
     $Last_Name = sanitise_input($_POST["Last_Name"]);
 } else {
     header("location: apply.php");
@@ -57,66 +59,66 @@ require_once ("settings.php");
 }
 
 if (isset($_POST["dob"])) {
-  $dob = sanitise_input($_POST["dob"]);
+    $dob = sanitise_input($_POST["dob"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Gender"])) {
-  $Gender = sanitise_input($_POST["Gender"]);
+    $Gender = sanitise_input($_POST["Gender"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Street_Address"])) {
-  $Street_Address = sanitise_input($_POST["Street_Address"]);
+    $Street_Address = sanitise_input($_POST["Street_Address"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Suburb_Town"])) {
-$Suburb_Town = sanitise_input($_POST["Suburb_Town"]);
+    $Suburb_Town = sanitise_input($_POST["Suburb_Town"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["State"])) {
-  $State = sanitise_input($_POST["State"]);
+    $State = sanitise_input($_POST["State"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Postcode"])) {
-  $Postcode = sanitise_input($_POST["Postcode"]);
+    $Postcode = sanitise_input($_POST["Postcode"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Email_Address"])) {
-  $Email_Address = sanitise_input($_POST["Email_Address"]);
+    $Email_Address = sanitise_input($_POST["Email_Address"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["Phone_Number"])) {
-  $Phone_Number = sanitise_input($_POST["Phone_Number"]);
+    $Phone_Number = sanitise_input($_POST["Phone_Number"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 if (isset($_POST["OtherSkills"])) {
-  $OtherSkills = sanitise_input($_POST["OtherSkills"]);
+    $OtherSkills = sanitise_input($_POST["OtherSkills"]);
 } else {
-  header("location: apply.php");
-  exit;
+    header("location: apply.php");
+    exit;
 }
 
 $Job_Reference = sanitise_input($Job_Reference);
@@ -124,37 +126,35 @@ $First_Name = sanitise_input($First_Name);
 $Last_Name = sanitise_input($Last_Name);
 $dob = sanitise_input($dob);
 $Gender = sanitise_input($Gender);
-$Street_Address= sanitise_input($Street_Address);
+$Street_Address = sanitise_input($Street_Address);
 $Suburb_Town = sanitise_input($Suburb_Town);
-$State= sanitise_input($State);
+$State = sanitise_input($State);
 $Postcode = sanitise_input($Postcode);
 $Email_Address = sanitise_input($Email_Address);
 $Phone_Number = sanitise_input($Phone_Number);
 $OtherSkills = sanitise_input($OtherSkills);
 
-// Make sure all the required fields are filled otherwise displays error message
-$errMsg = "";
-   if (empty($Job_Reference)) {
-    $errMsg .= "<p>You must enter job reference number.</p>";
+// Make sure all the required fields are filled; otherwise, display error message
+if (empty($Job_Reference)) {
+    $errMsg .= "<p>You must enter a job reference number.</p>";
 } elseif (!preg_match("/^[a-zA-Z0-9]{5}$/", $Job_Reference)) {
-    $errMsg .= "<p>Only five alphanumeric characters allowed in job reference number.</p>";
+    $errMsg .= "<p>Only five alphanumeric characters are allowed in the job reference number.</p>";
 }
 
-$errMsg = "";
-   if (empty($First_Name)) {
+
     $errMsg .= "<p>You must enter your first name.</p>";
 } elseif (!preg_match("/^[a-zA-Z]{1,20}$/", $First_Name)) {
-    $errMsg .= "<p>Only 20 alpha letters allowed in your first name.</p>";
-}
-  
-$errMsg = "";
-   if (empty($Last_Name)) {
-    $errMsg .= "<p>You must enter your last name.</p>";
-} elseif (!preg_match("/^[a-zA-Z]{1,20}$/", $Last_Name)) {
-    $errMsg .= "<p>Only 20 alpha letters allowed in your first name.</p>";
+    $errMsg .= "<p>Only 20 alphabetic letters are allowed in your first name.</p>";
 }
 
-$errMsg = "";
+
+if (empty($Last_Name)) {
+    $errMsg .= "<p>You must enter your last name.</p>";
+} elseif (!preg_match("/^[a-zA-Z]{1,20}$/", $Last_Name)) {
+    $errMsg .= "<p>Only 20 alphabetic letters are allowed in your last name.</p>";
+}
+
+
 if (empty($dob)) {
     $errMsg .= "<p>You must enter your date of birth.</p>";
 } else {
@@ -170,57 +170,107 @@ if (empty($dob)) {
     }
 }
 
-if (empty($Gender)) {
-  $errors[] = "Gender is required.";
-} else {
-  // Radio input has a value, you can proceed with further processing
-  $selectedGender = $_POST['Gender'];
-  }
 
-$errMsg = "";
-   if (empty($Street_Address)) {
-    $errMsg .= "<p>You must enter your Street address.</p>";
-} elseif (!preg_match("/^[a-zA-Z]{1,40}$/", $Street_Address)) {
-    $errMsg .= "<p>Only 40 alpha letters allowed in your Street address.</p>";
+if (empty($Gender)) {
+    $errMsg .= "<p>Gender is required.</p>";
+} else {
+    // Radio input has a value, you can proceed with further processing
+    $selectedGender = $_POST['Gender'];
 }
 
-$errMsg = "";
-   if (empty($Suburb_Town))) {
+
+if (empty($Street_Address)) {
+    $errMsg .= "<p>You must enter your street address.</p>";
+} elseif (!preg_match("/^[a-zA-Z]{1,40}$/", $Street_Address)) {
+    $errMsg .= "<p>Only 40 alphabetic letters are allowed in your street address.</p>";
+}
+
+
+if (empty($Suburb_Town)) {
     $errMsg .= "<p>You must enter your suburb or town.</p>";
 } elseif (!preg_match("/^[a-zA-Z]{1,40}$/", $Suburb_Town)) {
-    $errMsg .= "<p>Only 40 alpha letters allowed in suburb or town .</p>";
+    $errMsg .= "<p>Only 40 alphabetic letters are allowed in your suburb or town.</p>";
 }
 
 
+if (empty($State)) {
+    $errMsg .= "<p>You must select a state.</p>";
+}
+
+
+if (empty($Postcode)) {
+    $errMsg .= "<p>You must enter your postcode.</p>";
+} elseif (!preg_match("/^\d{4}$/", $Postcode)) {
+    $errMsg .= "<p>Postcode must be exactly 4 digits long.</p>";
   
-// Create the EOI table if it doesn't exist
-  $create_table_query = "CREATE TABLE IF NOT EXISTS EOI (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Job_Reference VARCHAR(5) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-    First_Name VARCHAR(20) NOT NULL,
-    Last_Name VARCHAR(20) NOT NULL,
-    dob DATE NOT NULL,
-    Gender VARCHAR(10) NOT NULL,
-    Street_Address VARCHAR(40) NOT NULL,
-    Suburb_Town_Town VARCHAR(40) NOT NULL,
-    State VARCHAR(3) NOT NULL,
-    Postcode VARCHAR(4) NOT NULL,
-    Email_Address VARCHAR(255) NOT NULL,
-    Phone_Number VARCHAR(12),
-    OtherSkills TEXT,
-    UNIQUE KEY(Job_Reference)
-  )";
-  $conn->query($create_table_query);
+}
+// Match postcode with state
+$postcodeState = array(
+  "3000" => "VIC",
+  "2000" => "NSW",
+  "4000" => "QLD",
+  "0800" => "NT",
+  "6000" => "WA",
+  "5000" => "SA",
+  "7000" => "TAS",
+  "2600" => "ACT",
+  
+);
+// Check if the entered postcode matches the selected state
+if (isset($State) && isset($Postcode)) {
+  $postcode = sanitise_input($Postcode);
+  $postcode = str_pad($postcode, 4, "0", STR_PAD_LEFT); // Pad the postcode with leading zeros if necessary
 
-  // Insert the EOI record into the table
-  $insert_query = "INSERT INTO EOI (Job_Reference, first_name, last_name, dob, Gender, Street_Address, Suburb_Town, State , Postcode, Email_Address, Phone_Number, OtherSkills) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  $stmt = $conn->prepare($insert_query);
-  $stmt->bind_param("ssssssssssss", $_POST["Job_Reference"], $_POST["first_name"], $_POST["last_name"], $_POST["dob"], $_POST["Gender"], $_POST["Street_Address"], $_POST["Suburb_Town"], $_POST["State"], $_POST["Postcode"], $_POST["Email_Address"], $_POST["Phone_Number"], $_POST["OtherSkills"]);
-
-  if ($stmt->execute()) {
-    // Retrieve the auto-generated EOInumber
-    $eoi_number = $stmt->insert_id;
+  if (!isset($postcodeState[$postcode])) {
+      $errMsg .= "<p>Invalid postcode.</p>";
+  } elseif ($postcodeState[$postcode] !== $State) {
+      $errMsg .= "<p>The selected state does not match the entered postcode.</p>";
   }
+}
+
+
+if (empty($Email_Address)) {
+    $errMsg .= "<p>You must enter your email address.</p>";
+} elseif (!filter_var($Email_Address, FILTER_VALIDATE_EMAIL)) {
+    $errMsg .= "<p>You must enter a valid email address.</p>";
+}
+
+
+if (empty($Phone_Number)) {
+    $errMsg .= "<p>You must enter your phone number.</p>";
+} elseif (!preg_match("/^(\+?\d{1,4}[\s-]?)?(?!0+\s+,?$)\d{10}\s*,?$/", $Phone_Number)) {
+    $errMsg .= "<p>You must enter a valid phone number (10 digits, no spaces or special characters).</p>";
+}
+
+ 
+// Create the EOI table if it doesn't exist
+$create_table_query = "CREATE TABLE IF NOT EXISTS EOI (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  Job_Reference VARCHAR(5) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  First_Name VARCHAR(20) NOT NULL,
+  Last_Name VARCHAR(20) NOT NULL,
+  dob DATE NOT NULL,
+  Gender VARCHAR(10) NOT NULL,
+  Street_Address VARCHAR(40) NOT NULL,
+  Suburb_Town VARCHAR(40) NOT NULL,
+  State VARCHAR(3) NOT NULL,
+  Postcode VARCHAR(4) NOT NULL,
+  Email_Address VARCHAR(255) NOT NULL,
+  Phone_Number VARCHAR(12),
+  OtherSkills TEXT,
+  UNIQUE KEY(Job_Reference)
+)";
+$conn->query($create_table_query);
+
+// Insert the EOI record into the table
+$insert_query = "INSERT INTO EOI (Job_Reference, first_name, last_name, dob, Gender, Street_Address, Suburb_Town, State , Postcode, Email_Address, Phone_Number, OtherSkills) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($insert_query);
+$stmt->bind_param("ssssssssssss", $_POST["Job_Reference"], $_POST["first_name"], $_POST["last_name"], $_POST["dob"], $_POST["Gender"], $_POST["Street_Address"], $_POST["Suburb_Town"], $_POST["State"], $_POST["Postcode"], $_POST["Email_Address"], $_POST["Phone_Number"], $_POST["OtherSkills"]);
+
+if ($stmt->execute()) {
+  // Retrieve the auto-generated EOInumber
+  $eoi_number = $stmt->insert_id;
+}
 ?>
 </body>
 </html>
