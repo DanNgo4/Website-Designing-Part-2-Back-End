dbef6763e4f1f4043463f4900f3ec3d1ef1b17a3
Yến Nhi Lâm Thanh <ninayal198@gmail.com>
2023-05-31 20:23:54 +1000

    Update manage.php

--- manage.php
+++ manage.php
@@ -36,7 +36,7 @@ if (isset($_SESSION["id"]) && isset($_SESSION["name"])) {
         <input type="hidden" name="action" value="list_by_position">
         <label for="Job_Reference">Job Reference:</label>
         <input type="text" name="Job_Reference" id="Job_Reference">
-        <input type="submit" value="List EOIs for Position">
+        <input type="submit" value="SUBMIT">
     </form>
     <hr>
     <h2>List EOIs For A Particular Applicant</h2>
@@ -48,7 +48,7 @@ if (isset($_SESSION["id"]) && isset($_SESSION["name"])) {
         <label for="Last_name">Last Name:</label>
         <input type="text" name="Last_name" id="Last_Name">
         <br>
-        <input type="submit" value="List EOIs for Applicant">
+        <input type="submit" value="SUBMIT">
     </form>
     <hr>
     <h2>Delete EOIs With A Specified Job Reference Number</h2>
@@ -56,7 +56,7 @@ if (isset($_SESSION["id"]) && isset($_SESSION["name"])) {
         <input type="hidden" name="action" value="delete_by_position">
         <label for="Job_Reference_delete">Job Reference:</label>
         <input type="text" name="Job_Reference" id="Job_Reference_delete">
-        <input type="submit" value="Delete EOIs">
+        <input type="submit" value="DELETE">
     </form>
     <hr>
     <h2>Change The Status Of An EOI</h2>
@@ -68,7 +68,7 @@ if (isset($_SESSION["id"]) && isset($_SESSION["name"])) {
         <label for="status">Status:</label>
         <input type="text" name="status" id="status">
         <br>
-        <input type="submit" value="Change Status">
+        <input type="submit" value="CHANGE">
     </form>
     <?php
     // Database connection
@@ -135,7 +135,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['action']) && $_GET['acti
 }
 }
 
-
     // List EOIs for a particular position (given a job reference number)
     function listEOIsForPosition($conn, $jobReference)
     {
@@ -155,5 +154,99 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['action']) && $_GET['acti
         } else {
             echo "<p class='error-message'>No EOIs found for position $jobReference.</p>";
         }
-    }
     // List EOIs for a particular applicant given their first name, last name, or both
+    function listEOIsForApplicant($conn, $firstName, $lastName)
+    {
+        $firstName = sanitizeInput($firstName);
+        $lastName = sanitizeInput($lastName);
+
+        $sql = "SELECT * FROM EOI WHERE First_Name LIKE '%$firstName%' AND Last_Name LIKE '%$lastName%'";
+        $result = $conn->query($sql);
+
+        // Display the results
+        if ($result->num_rows > 0) {
+            echo "<h2>EOIs for Applicant: $firstName $lastName</h2>";
+            echo "<table>";
+            echo "<tr><th>EOInumber</th><th>Job Reference</th><th>First Name</th><th>Last Name</th><th>Status</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["EOInumber"] . "</td><td>" . $row["Job_Reference"] . "</td><td>" . $row["First_Name"] . "</td><td>" . $row["Last_Name"] . "</td><td>" . $row["status"] . "</td></tr>";
+            }
+            echo "</table>";
+            echo "<p>EOIs for applicant $firstName $lastName listed successfully.</p>";
+        } else {
+            echo "<p>No EOIs found for applicant $firstName $lastName.</p>";
+        }
+    }
+
+    // Delete all EOIs with a specified job reference number
+    function deleteEOIsWithJobReference($conn, $jobReference)
+    {
+        $jobReference = sanitizeInput($jobReference);
+        $sql = "DELETE FROM EOI WHERE Job_Reference = '$jobReference'";
+        $result = $conn->query($sql);
+        if($result) {
+            echo "<p class='success-message'>EOIs for position $position deleted successfully.</p>";
+        } else {
+            echo "<p class='error-message'>Failed to delete EOIs for position $position.</p>";
+        }
+        }
+    // Change the Status of an EOI
+    function changeEOIStatus($conn, $EOinumber, $Status)
+    {
+        $EOInumber = sanitizeInput($EOInumber);
+        $Status = sanitizeInput($Status);
+
+        $sql = "UPDATE EOI SET status = '$Status' WHERE EOInumber = $EOInumber";
+        $result = $conn->query($sql);
+
+        if ($result) {
+            echo "<p class='success-message'>EOI status changed successfully.</p>";
+        } else {
+            echo "<p class='error-message'>Failed to change EOI status.</p>";
+        }
+    }ß
+
+    // Check the requested action and execute the appropriate query
+    if (isset($_GET['action'])) {
+        switch ($_GET['action']) {
+            case 'list_all':
+                $sortField = isset($_GET['sort_field']) ? $_GET['sort_field'] : 'EOInumber';
+                listAllEOIs($conn, $sortField);
+                break;
+            case 'list_by_position':
+                $jobReference = isset($GET['jobReference']) ? $_GET['jobReference'] : '';
+                listEOIsByPosition($conn, $jobReference);
+                break;
+                case 'list_by_applicant':
+                    $firstName = isset($_GET['firstName']) ? $_GET['firstName'] : '';
+                    $lastName = isset($_GET['lastName']) ? $_GET['lastName'] : '';
+                    listEOIsForApplicant($conn, $firstName, $lastName);
+                    break;                
+                case 'delete_by_position':
+                    $jobReference = isset($_GET['jobReference']) ? $_GET['jobReference'] : '';
+                    deleteEOIsByPosition($conn, $jobReference);
+                    break;
+                case 'change_status':
+                    $EOInumber = isset($_GET['EOInumber']) ? $_GET['EOInumer'] : '';
+                    $Status = isset($_GET['Status']) ? $_GET['Status'] : '';
+                    changeEOIStatus($conn, $EOInumber, $Status);
+                    break;
+                default:
+                    echo "<p class='error-message'>Invalid action.</p>";
+                    break;
+            }
+        }
+    
+    // Close the database connection
+    $conn->close();
+?>
+<a href = "logout.php"><h1>LOGOUT</h1></a>
+</body>
+</html>
+<?php 
+ } else{
+     header("Location: phpenhancements.php?error");
+     exit();
+}
+include 'Footer.inc';
+ ?>
